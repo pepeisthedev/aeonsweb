11067bdde3239197e774e6f388dc44d5d20a47b3
mdb1710 <mdb1710@gmail.com>
2024-05-06 12:57:59 -0400

    added assets to menu

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,6 +6,15 @@ import Image from 'next/image';
 const NavBar = () => {
   return (
     <nav className="sticky top-0 left-0 w-full bg-gray-800 text-gray-300 flex justify-between items-center p-4">
+      <div>
+        <Image 
+          className=''
+          src='/Aeonsmainlogo.png'
+          alt='Aeons logo'
+          width={130}
+          height={100}
+        />
+      </div>
       <ul className="flex justify-between w-full max-w-md mx-auto p-0 list-none">
         <li className="mr-6">
           <Link href="/" className="text-lg font-bold">
@@ -28,6 +37,22 @@ const NavBar = () => {
           </Link>
         </li>
       </ul>
+      <div className='flex flex-row justify-between'>
+      <Image 
+          className=''
+          src='/TwitterMenuLogo.png'
+          alt='Aeons logo'
+          width={60}
+          height={50}
+        />
+        <Image 
+        className=''
+        src='/discordlogo.png'
+        alt='Aeons logo'
+        width={60}
+        height={50}
+      />
+      </div>
     </nav>
   );
 };
