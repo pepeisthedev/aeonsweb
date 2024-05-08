02c018d701e3cbd0c5f754bd2697899e315fa6f2
mdb1710 <mdb1710@gmail.com>
2024-05-08 15:14:57 -0400

    resize photo

--- app/components/NewHero.tsx
+++ app/components/NewHero.tsx
@@ -3,19 +3,19 @@ import Image from 'next/image'
 
 const NewHero = () => {
     return(
-        <div className=''>
-            <p className='text-5xl text-black'>New Hero</p>
-            <div className=''>
+        <div className='flex flex-col items-center justify-center space-y-0 '>
+            <p className='text-9xl text-black'>New Hero</p>
+            <div className='size-96'>
                 <Image 
                     src='/3lineslogo.png'
                     alt='3 lines'
                     className='z-10'
-                    width={600}
-                    height={600}
+                    width={400}
+                    height={400}
                       priority
                 />
             </div>
-            <p className='text-5xl text-black'>New Hero</p>
+            <p className='text-9xl text-black'>New Hero</p>
         </div>
     )
 }
