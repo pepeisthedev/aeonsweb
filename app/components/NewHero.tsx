6c72a41bce2bd8447554ef171d6ec192e17658b9
mdb1710 <mdb1710@gmail.com>
2024-05-08 19:37:01 -0400

    fix: image sizing

--- app/components/NewHero.tsx
+++ app/components/NewHero.tsx
@@ -3,19 +3,23 @@ import Image from 'next/image'
 
 const NewHero = () => {
     return(
-        <div className='flex flex-col items-center justify-center space-y-0 '>
-            <p className='text-9xl text-black'>New Hero</p>
-            <div className='size-96'>
+        <div className='flex flex-col items-center justify-center'>
+            <div className='border-2 border-red-500 z-20'>
+                <p className='text-9xl text-black'>Explore</p>
+            </div>
+            <div className='border-2 border-red-500'>
                 <Image 
-                    src='/3lineslogo.png'
+                    src='/3linescopy.png'
                     alt='3 lines'
-                    className='z-10'
-                    width={400}
-                    height={400}
+                    className=''
+                   width={600}
+                   height={650}
                       priority
                 />
             </div>
-            <p className='text-9xl text-black'>New Hero</p>
+            <div className='border-2 border-red-500 z-20'>
+                <p className='text-9xl text-black'>Art</p>
+            </div>
         </div>
     )
 }
