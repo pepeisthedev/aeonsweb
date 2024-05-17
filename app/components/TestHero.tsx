cfaa63dab3342d21e3fe45f3b6401873bcf08daf
mdb1710 <mdb1710@gmail.com>
2024-05-17 16:01:50 -0400

    added text to tooltip

--- app/components/TestHero.tsx
+++ app/components/TestHero.tsx
@@ -11,11 +11,11 @@ const TestHero = () => {
                 <div className='z-20'>
                     <p className='text-9xl text-black uppercase opacity-25'>Explore</p>
                 </div>
-            <Tooltip content='Aeons - Aeons are the eternal guardians of art, capturing the timeless essence of creativity.' position='right' className='w-full text-left align-center'>
+            <Tooltip content='Aeons - Aeons are the eternal guardians of art, capturing the timeless essence of creativity.' position='bottom' className='w-full text-left align-center'>
                 <span className='text-sm text-black uppercase opacity-0'>test</span>    
             </Tooltip>
-            <Tooltip content='hello world' position='bottom'>
-                <div className=''>
+            <Tooltip content='Humans : This line reflects humanitys ongoing role and evolution within the tapestry of life and art' position='top' className='w-full text-left align-center'>
+                <div className='w-full'>
                     <Image 
                         src='/RealMovingLogo.gif'
                         alt='3 lines'
@@ -26,7 +26,7 @@ const TestHero = () => {
                     />
                 </div>
             </Tooltip>
-            <Tooltip content='Aeons - Aeons are the eternal guardians of art, capturing the timeless essence of creativity.' position='left' className='w-96'>
+            <Tooltip content='Digital :  The Digital Realm, highlighted by the last line through the narrative of Bitcoin, represents the cutting-edge evolution of technology and its impact on society.' position='top' className='w-full text-left align-center'>
                 <span className='text-sm text-black uppercase opacity-0'>test</span>    
             </Tooltip>
             <div className='z-20'>
