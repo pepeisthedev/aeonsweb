034d573a4bd3b471398d1acf4791abff65f2e92f
mdb1710 <mdb1710@gmail.com>
2024-05-18 12:16:04 -0400

    fix: new homepage logo

--- app/components/NewHero.tsx
+++ app/components/NewHero.tsx
@@ -4,20 +4,20 @@ import Image from 'next/image'
 const NewHero = () => {
     return(
         <div className='flex flex-col items-center justify-center'>
-            <div className='z-20'>
+            <div className='z-20 -mb-10'>
                 <p className='text-9xl text-white uppercase'>Explore</p>
             </div>
             <div className=''>
                 <Image 
-                    src='/RealMovingLogo.gif'
+                    src='/AnimatedlogoNew.png'
                     alt='3 lines'
                     className=''
-                   width={500}
-                   height={550}
+                   width={400}
+                   height={450}
                       priority
                 />
             </div>
-            <div className='z-20'>
+            <div className='z-20 -mt-10'>
                 <p className='text-9xl text-white uppercase'>Art</p>
             </div>
         </div>
