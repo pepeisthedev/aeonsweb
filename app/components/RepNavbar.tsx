5c26e4430842dc9178de1d63603de32fff8e7e64
mdb1710 <mdb1710@gmail.com>
2024-06-09 20:51:35 -0400

    added hamburger menu

--- app/components/RepNavbar.tsx
+++ app/components/RepNavbar.tsx
@@ -1,21 +1,45 @@
+'use client'
+
 import Link from "next/link";
 import Image from "next/image";
 
+import { useState } from "react";
+
+// width={180} height={150}
+
 const RepNavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+      setIsOpen(!isOpen);
+      
+  }
+
   return(
     <nav className="p-2 sm:p-6 md:flex md:justify-between md:items-center">
       <div className="container mx-auto flex justify-between items-center">
-      <div>
-        <Link href="/">
+      <div className="relative h-16 w-48">
+        <Link href="/" >
           <Image 
             className=''
             src='/Aeonsmainlogo.png'
             alt='Aeons logo'
-            width={180}
-            height={150}
+            fill
           />
         </Link>
       </div>
+      <section className="lg:hidden md:hidden flex">
+      <div>
+            <button onClick={handleClick} className="flex flex-col justify-center items-center">
+                <span className={`bg-[#e6a40e] block transition-all duration-300 ease-out 
+                    h-0.5 w-6 rounded-sm`}></span>
+                <span className={`bg-[#e6a40e] block transition-all duration-300 ease-out 
+                    h-0.5 w-6 rounded-sm my-0.5`}></span>
+                <span className={`bg-[#e6a40e] block transition-all duration-300 ease-out 
+                    h-0.5 w-6 rounded-sm`}></span>  
+            </button>
+        </div>
+      </section>
       <div className="hidden md:flex md:items-center">
         <Link href="/gallery" className="text-4xl font-bold text-center uppercase mx-2">
             <span>Galle<span className='text-[#e6a40e]'>ry</span></span>
@@ -25,7 +49,7 @@ const RepNavBar = () => {
         </Link>
         <Link href='https://twitter.com/AeonsBTC' >
             <Image 
-                className='mx-2'
+                className=''
                 src='/AeonsTwitter60x60.png'
                 alt='Aeons logo'
                 width={60}
@@ -34,7 +58,7 @@ const RepNavBar = () => {
         </Link>
         <Link href='https://discord.gg/6KucTavSDh'>
               <Image 
-                className='mx-2'
+                className=''
                 src='/AeonsDiscord60x60.png'
                 alt='Aeons logo'
                 width={60}
