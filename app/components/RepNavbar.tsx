abee26f63bb9401dc2219f50137f73bf5341b358
mdb1710 <mdb1710@gmail.com>
2024-06-09 21:39:39 -0400

    added dropdown menu

--- app/components/RepNavbar.tsx
+++ app/components/RepNavbar.tsx
@@ -12,7 +12,7 @@ const RepNavBar = () => {
 
   const handleClick = () => {
       setIsOpen(!isOpen);
-      
+      console.log(isOpen)
   }
 
   return(
@@ -38,6 +38,50 @@ const RepNavBar = () => {
                 <span className={`bg-[#e6a40e] block transition-all duration-300 ease-out 
                     h-0.5 w-6 rounded-sm`}></span>  
             </button>
+
+            <div className={isOpen ? "showMenuNav" : "hideMenuNav"}>
+                <div
+                  className="absolute top-0 right-0 px-8 py-8"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <svg
+                    className="h-8 w-8 text-gray-600"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="18" y1="6" x2="6" y2="18" />
+                    <line x1="6" y1="6" x2="18" y2="18" />
+                  </svg>
+                </div>
+                <ul className="flex flex-col items-center justify-between min-h-[250px]">
+                    <li className="border-b border-gray-400 my-8 uppercase">
+                        <Link href="/gallery" className="text-4xl font-bold text-center uppercase mx-2">
+                          <span>Galle<span className='text-[#e6a40e]'>ry</span></span>
+                        </Link>
+                    </li>
+                    <li className="border-b border-gray-400 my-8 uppercase">
+                        <Link href="/wlchecker" className='text-4xl font-bold text-center text-nowrap uppercase mx-2'>
+                          WL Check<span className='text-[#e6a40e]'>er</span>
+                        </Link>
+                    </li>
+                    <li className="border-b border-gray-400 my-8 uppercase">
+                        <Link href="https://twitter.com/AeonsBTC" className="text-4xl font-bold text-center uppercase mx-2">
+                          <span>Twitt<span className='text-[#e6a40e]'>er</span></span>
+                        </Link>
+                    </li>
+                    <li className="border-b border-gray-400 my-8 uppercase">
+                        <Link href="https://discord.gg/6KucTavSDh" className="text-4xl font-bold text-center uppercase mx-2">
+                          <span>Disco<span className='text-[#e6a40e]'>rd</span></span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+
+            
         </div>
       </section>
       <div className="hidden md:flex md:items-center">
@@ -67,6 +111,25 @@ const RepNavBar = () => {
           </Link>
       </div>
       </div>
+      <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: black;
+        z-index: 20;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
     </nav>
   )
 }
