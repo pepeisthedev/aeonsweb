5a24eebf7e351faffd76d3569de5c39f25617f97
mdb1710 <mdb1710@gmail.com>
2024-06-11 17:23:21 -0400

    fix: removed tooltip and z-index

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -9,9 +9,9 @@ import Tooltip from '../components/Tooltip'
 const Gallery = () => {
     return(
         <main className="flex h-screen flex-col items-center justify-center bg-cover bg-[url('/blackaeonsbg.png')] -my-12">
-            <Tooltip content='Will this work?'>
+            
                 <ComingSoon />
-            </Tooltip>
+            
         </main>
     )
 }
