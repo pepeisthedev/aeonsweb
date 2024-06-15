97d0c9b42c30399ab77f7bd3e070ce1b809a27c3
Pepe Is The Dev <pepeisthedev@gmail.com>
2024-06-15 18:42:03 +0200

    Swipe cards

--- app/components/gallery/DetailsModal.tsx
+++ app/components/gallery/DetailsModal.tsx
@@ -1,6 +1,7 @@
-import React, {ReactNode, MouseEventHandler, useEffect} from 'react';
+import React, {ReactNode, MouseEventHandler, useEffect, useState} from 'react';
 import { useSwipeable } from 'react-swipeable';
 import './DetailsModal.css';
+import './FlipDetailsModal.css';
 
 interface ModalProps {
     children: ReactNode;
@@ -10,6 +11,8 @@ interface ModalProps {
 }
 
 const DetailsModal: React.FC<ModalProps> = ({ children, onClose , onNext, onPrevious}) => {
+    const [swipeOut, setSwipeOut] = useState(''); // Change this state to a string
+
     const handleOverlayClick = (e: React.MouseEvent) => {
         if (e.target === e.currentTarget) {
             onClose(e);
@@ -39,13 +42,29 @@ const DetailsModal: React.FC<ModalProps> = ({ children, onClose , onNext, onPrev
     }, [onNext, onPrevious]);
 
     const handlers = useSwipeable({
-        onSwipedLeft: () => onNext(),
-        onSwipedRight: () => onPrevious()
+        onSwipedLeft: () => {
+            setSwipeOut('swipeOutLeft');
+            setTimeout(() => {
+                onNext();
+            }, 100); // Change this delay to 0.25s
+            setTimeout(() => {
+                setSwipeOut('');
+            }, 200);
+        },
+        onSwipedRight: () => {
+            setSwipeOut('swipeOutRight');
+            setTimeout(() => {
+                onPrevious();
+            }, 100); // Change this delay to 0.25s
+            setTimeout(() => {
+                setSwipeOut('');
+            }, 200);
+        }
     });
 
     return (
         <div {...handlers} className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 modal-container" onClick={handleOverlayClick}>
-            <div className="relative modal-transparent p-6 rounded shadow-lg"> {/* Set position to relative */}
+            <div className={`relative modal-transparent p-6 rounded shadow-lg ${swipeOut}`}> {/* Use the swipeOut state here */}
                 <img src="/arrow-right.png" alt="Previous" className="modal-button left" onClick={onPrevious} />
                 <img src="/arrow-right.png" alt="Next" className="modal-button right" onClick={onNext} />
                 {children}
