847b3dddabd572cd4a96d92b88326d45e77940a1
Pepe Is The Dev <pepeisthedev@gmail.com>
2024-06-15 11:33:20 +0200

    Add swipe and arrow button support

--- app/components/gallery/DetailsModal.tsx
+++ app/components/gallery/DetailsModal.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode, MouseEventHandler } from 'react';
+import React, {ReactNode, MouseEventHandler, useEffect} from 'react';
+import { useSwipeable } from 'react-swipeable';
 import './DetailsModal.css';
 
 interface ModalProps {
@@ -15,8 +16,35 @@ const DetailsModal: React.FC<ModalProps> = ({ children, onClose , onNext, onPrev
         }
     };
 
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            switch (e.key) {
+                case 'ArrowRight':
+                    onNext();
+                    break;
+                case 'ArrowLeft':
+                    onPrevious();
+                    break;
+                default:
+                    break;
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        // Remove event listeners on cleanup
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [onNext, onPrevious]);
+
+    const handlers = useSwipeable({
+        onSwipedLeft: () => onNext(),
+        onSwipedRight: () => onPrevious()
+    });
+
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 modal-container" onClick={handleOverlayClick}>
+        <div {...handlers} className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 modal-container" onClick={handleOverlayClick}>
             <div className="relative modal-transparent p-6 rounded shadow-lg"> {/* Set position to relative */}
                 <img src="/arrow-right.png" alt="Previous" className="modal-button left" onClick={onPrevious} />
                 <img src="/arrow-right.png" alt="Next" className="modal-button right" onClick={onNext} />
