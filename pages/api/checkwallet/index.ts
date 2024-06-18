5381a1e49a0111a18b4ccc63e4a472c2a29ce5b0
Pepe Is The Dev <pepeisthedev@gmail.com>
2024-06-18 22:29:34 +0200

    optimize performance reading csv

--- pages/api/checkwallet/index.ts
+++ pages/api/checkwallet/index.ts
@@ -9,7 +9,27 @@ interface CsvRow {
     FCFS: string;
 }
 
+let VIPs: Record<string, CsvRow> = {};
+let FCFSs: Record<string, CsvRow> = {};
+
+const readCSV = new Promise<void>((resolve, reject) => {
+    fs.createReadStream(path.join(process.cwd(), 'data', 'whitelist.csv'))
+        .pipe(csv())
+        .on('data', (data: CsvRow) => {
+            if (data.VIP) {
+                VIPs[data.VIP] = data;
+            }
+            if (data.FCFS) {
+                FCFSs[data.FCFS] = data;
+            }
+        })
+        .on('end', resolve)
+        .on('error', reject);
+});
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    await readCSV;
+
     const { address } = req.body;
 
     // Check if address is empty
@@ -17,29 +37,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(404).json({ error: 'Wallet address is required' });
     }
 
-    const results: CsvRow[] = [];
+    let VIP = '0';
+    let FCFS = '0';
 
-    fs.createReadStream(path.join(process.cwd(), 'data', 'whitelist.csv'))
-        .pipe(csv())
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-            const VIPRecord = results.find((r) => r.VIP === address);
-            const FCFSRecord = results.find((r) => r.FCFS === address);
+    if (VIPs[address]) {
+        VIP = '1';
+        FCFS = '1';
+    } else if (FCFSs[address]) {
+        FCFS = '1';
+    }
 
-            if (VIPRecord || FCFSRecord) {
-                let VIP = '0';
-                let FCFS = '0';
-
-                if (VIPRecord) {
-                    VIP = '1';
-                    FCFS = '1';
-                } else if (FCFSRecord) {
-                    FCFS = '1';
-                }
-
-                res.status(200).json({ VIP, FCFS });
-            } else {
-                res.status(404).json({ error: 'Wallet not found' });
-            }
-        });
+    if (VIP === '1' || FCFS === '1') {
+        res.status(200).json({ VIP, FCFS });
+    } else {
+        res.status(404).json({ error: 'Wallet not found' });
+    }
 }
