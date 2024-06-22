eed678b9e0903d4d15f1f610406d35b93afc6240
Pepe Is The Dev <pepeisthedev@gmail.com>
2024-06-22 14:09:47 +0200

    og metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { League_Gothic } from "next/font/google";
+import {Inter} from "next/font/google";
+import {League_Gothic} from "next/font/google";
 import "./globals.css";
 
 import Head from "next/head";
@@ -8,51 +7,40 @@ import Head from "next/head";
 import localFont from 'next/font/local'
 
 import Nav from './components/Nav'
-import NavBar from "./components/Nav";
-import MobileNav from "./components/MobileNav";
-import NewNav from "./components/NewNav";
-import RepNavBar from "./components/RepNavbar";
-import background from '../public/blackaeonsbg.png'
-import TestNavBar from "./components/TestNavbar";
 
-const inter = Inter({ subsets: ["latin"] });
-const league_gothic = League_Gothic({ subsets: ["latin"] })
+const inter = Inter({subsets: ["latin"]});
+const league_gothic = League_Gothic({subsets: ["latin"]})
 
 const edo = localFont({
-  src: [
-    {
-      path: '../public/fonts/edo.ttf',
-      weight: '800',
-    },
-  ],
-  variable: '--font-edo',
+    src: [
+        {
+            path: '../public/fonts/edo.ttf',
+            weight: '800',
+        },
+    ],
+    variable: '--font-edo',
 })
 
-export const metadata: Metadata = {
-  title: "Aeons YEAH",
-  description: "Testing",
-  icons: {
-    icon: '/favicon.ico'
-  },
-  openGraph: {
-    images: ['/Aeons_Preview.png'],
-  }
-};
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      
-      <body className={`${edo.variable} ${inter.className} ${league_gothic.className}`}>
-      
-       
-        <Nav />
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <Head>
+            <title>Aeons new preview title</title>
+            <meta name="description" content="Testing an description"/>
+            <link rel="icon" href="/favicon.ico"/>
+            <meta property="og:image" content="/Aeons_Preview.png"/>
+            <meta property="og:description" content="This is an Awesome project"/>
+            <meta property="og:url" content="https://bro-check-this-gallery.vercel.app"/>
+        </Head>
+        <body className={`${edo.variable} ${inter.className} ${league_gothic.className}`}>
+            <Nav/>
+            {children}
+        </body>
+        </html>
+    );
 }
